ad727cd35d05c5a1980dec865f4e6a9d1504a631
bleavs <bleavitt@colgate.edu>
2017-09-01 15:33:08 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get 'students', to: 'students#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,6 +41,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
+
 
   # Example resource route with concerns:
   #   concern :toggleable do
